14c53bfb8b628989b25f660f8bd5c55a3dc6e34c
Adarsh-Teaching <147122925+adarshtrainer@users.noreply.github.com>
2026-01-04 17:43:15 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mysql = require("mysql2");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
+
+// RDS Connection
+const db = mysql.createConnection({
+  // mysql://admin:**********@database-1.xyz.ap-south-1.rds.amazonaws.com:4021/db
+  // NOTE FOR APP TESTING: CHNAGE DB CONNECTION URL AS BELOW
+  host: "database-1.xyz.ap-south-1.rds.amazonaws.com",
+  user: "admin",
+  password: "261997Adi#",
+  database: "db",
+  port: 4021,
+});
+
+// Connect
+db.connect((err) => {
+  if (err) {
+    console.error("❌ DB Connection Error:", err);
+    return;
+  }
+  console.log("✅ Connected to AWS RDS MySQL");
+});
+
+// Show data + form
+app.get("/", (req, res) => {
+  db.query("SELECT * FROM users", (err, results) => {
+    if (err) throw err;
+    res.render("index", { users: results });
+  });
+});
+
+// Insert data
+app.post("/add", (req, res) => {
+  const { name, email } = req.body;
+
+  const sql = "INSERT INTO users (name, email) VALUES (?, ?)";
+  db.query(sql, [name, email], (err) => {
+    if (err) throw err;
+    res.redirect("/");
+  });
+});
+
+// Start server
+app.listen(3000, () => {
+  console.log("🚀 App running at http://localhost:3000");
+});
